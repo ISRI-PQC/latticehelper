d0c385f819e171d3291f251a6a537a5219ee6ff6
Petr Muzikant <petr.muzikant@cyber.ee>
2024-05-20 09:45:00 +0200

    gitignore and finalizing

--- old/pqdevkit/include/pqdevkit.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PQDEVKIT_HPP
-#define PQDEVKIT_HPP
-
-#include "poly_matrix.hpp"
-#include "poly_proxy.hpp"
-#include "poly_vector.hpp"
-#include "pqdevkit_math_lib.hpp"
-
-#endif // PQDEVKIT_HPP
